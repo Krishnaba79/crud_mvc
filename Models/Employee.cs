544480eef6906673961863c77ea3b6cb7cb99785
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-04-29 12:21:34 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace crud_mvc.Models
+{
+    public class Employee
+    {
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public required string Department { get; set; }
+        public Char Gender { get; set; }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿namespace crud_mvc.Models
+{
+    public class Repository
+    {
+        private static List<Employee> allEmpoyees = new List<Employee>();
+        public static IEnumerable<Employee> AllEmpoyees
+        {
+            get { return allEmpoyees; }
+        }
+        public static void Create(Employee Employee)
+        {
+            allEmpoyees.Add(Employee);
+        }
+        public static void Delete(Employee Employee)
+        {
+            allEmpoyees.Remove(Employee);
+        }
+
+    }
+}
